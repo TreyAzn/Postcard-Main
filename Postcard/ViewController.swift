d5e916e88de64dbe31c082a92006bc7fb15fd410
2A Morris T <2amorrist@002-iLoop.local>
2015-09-09 11:42:42 -0400

    first commit

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by 2A Morris T on 8/25/15.
+//  Copyright © 2015 2A Morris T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterAMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton)
+        {
+        // Adding a commnet here to test commit
+        // Code will evaluate when we press the button.
+        messageLabel.hidden = false
+        messageLabel.text = enterAMessageTextField.text
+        enterAMessageTextField.text = ""
+        enterAMessageTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.greenColor()
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.redColor()
+        enterNameTextField.text = ""
+            
+            
+        
+    }
+    
+
+}
+
